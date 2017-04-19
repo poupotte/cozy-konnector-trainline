72221b0ada30ca3d9eb79c2be943bffecab15b74
doubleface <christophe@thiriot.xyz>
2017-04-19 18:27:40 +0200

    cozy-konnector-libs update to run in a real cozy-stack

--- data/env_development.js
+++ data/env_development.js
@@ -4,5 +4,7 @@ const tokenPath = path.join(__dirname, 'token.json')
 module.exports = {
   COZY_CREDENTIALS: fs.existsSync(tokenPath) ? fs.readFileSync(tokenPath) : 'NO TOKEN',
   COZY_URL: 'http://cozy.tools:8080',
+  NODE_ENV: 'development',
   COZY_FIELDS: '{"login": "login", "password": "password", "folderPath": "folderPath"}'
+  DEBUG: '*'
 }
