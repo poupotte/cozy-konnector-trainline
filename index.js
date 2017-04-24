34d7adf0cbf74e249cd7a69960385f8b71ecc52b
doubleface <christophe@thiriot.xyz>
2017-04-24 17:05:57 +0200

    New adapations to be runnable in cozy-stack

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const konnector = require('./konnector')
 
 const cozyFields = JSON.parse(process.env.COZY_FIELDS)
 
-konnector.fetch(cozyFields, err => {
+konnector.fetch({account: cozyFields.account, folderPath: cozyFields.folder_to_save}, err => {
   console.log('The konnector has been run')
   if (err) console.log(err, 'There was an error')
 })
