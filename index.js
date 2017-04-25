29bfb3ebe478a9f0340c871e5ca1a1a6f9c3889c
doubleface <christophe@thiriot.xyz>
2017-04-25 18:29:07 +0200

    Now exit node process as error when a javascript error occured

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const cozyFields = JSON.parse(process.env.COZY_FIELDS)
 
 konnector.fetch({account: cozyFields.account, folderPath: cozyFields.folder_to_save}, err => {
   console.log('The konnector has been run')
-  if (err) console.error(err, 'There was an error')
+  if (err) {
+    console.error(err, 'There was an error')
+    process.exit(1)
+  }
 })
