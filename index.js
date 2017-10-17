a22c9200ed5558b276894a8b8fa9208c3968b662
doubleface <christophe@thiriot.xyz>
2017-10-17 11:08:51 +0200

    polish: now use promise based code and better error handling

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict'
 
-const {log, BaseKonnector, saveBills} = require('cozy-konnector-libs')
-const requestJson = require('request-json')
+const {log, BaseKonnector, saveBills, request} = require('cozy-konnector-libs')
 const moment = require('moment')
+let rq = request({
+  // debug: true
+})
 
 // The goal of this connector is to fetch bills from the
 // service trainline.fr
@@ -16,60 +18,57 @@ module.exports = new BaseKonnector(function fetch (fields) {
 })
 
 const baseUrl = 'https://www.captaintrain.com/'
-const client = requestJson.createClient(baseUrl)
-const userAgent = 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:37.0) ' +
-                  'Gecko/20100101 Firefox/37.0'
 
-client.headers['user-agent'] = userAgent
 function login (fields) {
-  const data = {}
-  return new Promise((resolve, reject) => {
-    // Signin form
-    const signinForm = {
-      concur_auth_code: null,
-      concur_migration_type: null,
-      concur_new_email: null,
-      correlation_key: null,
-      email: fields.login,
-      facebook_id: null,
-      facebook_token: null,
-      google_code: null,
-      google_id: null,
-      password: fields.password,
-      source: null,
-      user_itokend: null
+  // Signin form
+  const signinForm = {
+    concur_auth_code: null,
+    concur_migration_type: null,
+    concur_new_email: null,
+    correlation_key: null,
+    email: fields.login,
+    facebook_id: null,
+    facebook_token: null,
+    google_code: null,
+    google_id: null,
+    password: fields.password,
+    source: null,
+    user_itokend: null
+  }
+  // Signin
+  const signinPath = `${baseUrl}api/v5/account/signin`
+  return rq({
+    uri: signinPath,
+    method: 'POST',
+    form: signinForm,
+    resolveWithFullResponse: true
+  })
+  .then(res => {
+    log('info', 'Connected')
+    if (res.statusCode === 422) {
+      throw new Error('LOGIN_FAILED')
     }
-    // Signin
-    const signinPath = `${baseUrl}api/v5/account/signin`
-    client.post(signinPath, signinForm, (err, res, body) => {
-      if (err) {
-        return reject(err)
+    // Retrieve token
+    const token = res.body.meta.token
+
+    rq = rq.defaults({
+      headers: {
+        Authorization: `Token token="${token}"`
       }
-      log('info', 'Connected')
-      if (res.statusCode === 422) {
-        return reject(new Error('LOGIN_FAILED'))
+    })
+
+    // the api/v5/pnrs uri gives all information necessary to get bill
+    // information
+    return rq(`${baseUrl}api/v5/pnrs`)
+    .then(body => {
+      const data = {}
+      // We check there are bills
+      if (body.proofs && body.proofs.length > 0) {
+        saveMetadata(data, body)
+        return getNextMetaData(computeNextDate(body.pnrs), data)
+      } else {
+        return data
       }
-      // Retrieve token for json client
-      const token = body.meta.token
-      //  client.headers.cookie = cookie;
-      client.headers.Authorization = `Token token="${token}"`
-      data.authHeader = `Token token="${token}"`
-      // the api/v5/pnrs uri gives all information necessary to get bill
-      // information
-      client.get(`${baseUrl}api/v5/pnrs`, (err, res, body) => {
-        if (err) {
-          return reject(err)
-        }
-        // We check there are bills
-        if (body.proofs && body.proofs.length > 0) {
-          saveMetadata(data, body)
-          getNextMetaData(computeNextDate(body.pnrs), data)
-          .then(() => resolve(data))
-          .catch(err => reject(err))
-        } else {
-          return resolve(data)
-        }
-      })
     })
   })
 }
@@ -91,20 +90,14 @@ function computeNextDate (pnrs) {
 }
 
 function getNextMetaData (startdate, data) {
-  return new Promise((resolve, reject) => {
-    client.get(`${baseUrl}api/v5/pnrs?date=${startdate}`, (err, res, body) => {
-      if (err) {
-        return reject(err)
-      }
-      if (body.proofs && body.proofs.length > 0) {
-        saveMetadata(data, body)
-        getNextMetaData(computeNextDate(body.pnrs), data)
-        .then(() => resolve())
-        .catch(err => reject(err))
-      } else {
-        resolve()
-      }
-    })
+  return rq(`${baseUrl}api/v5/pnrs?date=${startdate}`)
+  .then(body => {
+    if (body.proofs && body.proofs.length > 0) {
+      saveMetadata(data, body)
+      return getNextMetaData(computeNextDate(body.pnrs), data)
+    } else {
+      return data
+    }
   })
 }
 
@@ -154,137 +147,136 @@ function saveMetadata (data, body) {
 }
 
 function fetchBills (data) {
-  return new Promise((resolve, reject) => {
-    const bills = []
-    // List of already managed proofs
-    const managedProofId = []
-    for (const proof of data.proofs) {
-      if (!proof.url) {
-        // No need to go further.
-        continue
+  const bills = []
+  // List of already managed proofs
+  const managedProofId = []
+  for (const proof of data.proofs) {
+    if (!proof.url) {
+      // No need to go further.
+      continue
+    }
+
+    // The proof can be duplicated, we only manage the one which were not taken
+    // care of already.
+    if (managedProofId.indexOf(proof.id) !== -1) {
+      // This proof is already dealt with
+      continue
+    } else {
+      // Add to the managed proof list
+      managedProofId.push(proof.id)
+    }
+
+    // A bill can be linked to several pnrs, we retrieve all of them
+    // For some unknown reason, some users don't have pnrs backlinked to
+    // proofs, let's initialize the array with the one linked to the proof.
+    let linkedPNR = [data.pnrs.find(pnr => pnr.id === proof.pnr_id)]
+    try {
+      linkedPNR = data.pnrs.filter(
+        pnr => pnr.proof_ids instanceof Array && pnr.proof_ids.indexOf(proof.id) !== -1
+      )
+    } catch (e) {
+      // We do nothing with the error as linkedPNR is set anyway.
+      log('warning', 'linkedPNR')
+      console.log(e, 'linkedPNR error')
+      log('warning', e)
+    }
+
+    // For some unknown reason, some users don't have system set for the pnr.
+    // By default we set it to sncf
+    linkedPNR = linkedPNR.map((pnr) => {
+      if (typeof pnr.system === 'undefined') {
+        pnr.system = 'sncf'
+      }
+      return pnr
+    })
+
+    // We try to find the list of the systems. there will be one
+    // bankoperation/proof/system
+    const systems = linkedPNR.reduce((sys, pnr) => {
+      if (sys.indexOf(pnr.system) === -1) {
+        return sys.concat(pnr.system)
+      }
+      return sys
+    }, [])
+
+    // Calculate the amount of each system because their is one operation per
+    // system.
+    for (const system of systems) {
+      const bill = {
+        pdfurl: proof.url,
+        type: 'train',
+        vendor: 'Trainline',
+        system,
+        date: moment(proof.created_at).hours(0)
+                                      .minutes(0)
+                                      .seconds(0)
+                                      .milliseconds(0)
       }
 
-      // The proof can be duplicated, we only manage the one which were not taken
-      // care of already.
-      if (managedProofId.indexOf(proof.id) !== -1) {
-        // This proof is already dealt with
-        continue
+      // Get the list of refunds for the current bill
+      let refundID = []
+      refundID = linkedPNR.filter(pnr => pnr.system === system).reduce(
+        (list, pnr) => list.concat(pnr.after_sales_log_ids), []
+      )
+      let listRefund = []
+      listRefund = refundID.reduce((list, id) => list.concat(
+        data.after_sales_logs.find(asl => asl.id === id)), []
+      )
+
+      if (proof.type === 'purchase') {
+        // Compute the sum of refunds for the current bill
+        const reinboursedAmount = listRefund.reduce(
+          (sum, rb) => sum - rb.added_cents + rb.refunded_cents, 0
+        )
+        // We compute the amount of not reimbursed trips.
+        const paidAmount =
+          linkedPNR.filter(pnr => pnr.system === system).reduce(
+            (sum, p) => sum + p.cents, 0
+          )
+        // Get the the sum of penalties
+        const penaltiesAmount = listRefund.reduce(
+          (sum, rb) => sum + rb.penalty_cents, 0)
+        bill.amount = (paidAmount + reinboursedAmount + penaltiesAmount) / 100
       } else {
-        // Add to the managed proof list
-        managedProofId.push(proof.id)
+        // Find the unique Refund based on the emission date
+        const refund = listRefund.find(
+          refund => refund.date === proof.created_at
+        )
+        bill.amount = (refund.refunded_cents - refund.added_cents) / 100
+        bill.isRefund = true
       }
 
-      // A bill can be linked to several pnrs, we retrieve all of them
-      // For some unknown reason, some users don't have pnrs backlinked to
-      // proofs, let's initialize the array with the one linked to the proof.
-      let linkedPNR = [data.pnrs.find(pnr => pnr.id === proof.pnr_id)]
-      try {
-        linkedPNR = data.pnrs.filter(
-          pnr => pnr.proof_ids instanceof Array && pnr.proof_ids.indexOf(proof.id) !== -1
-        )
-      } catch (e) {
-        // We do nothing with the error as linkedPNR is set anyway.
-        log('error', 'linkedPNR')
-        log('error', e)
-      }
-
-      // For some unknown reason, some users don't have system set for the pnr.
-      // By default we set it to sncf
-      linkedPNR = linkedPNR.map((pnr) => {
-        if (typeof pnr.system === 'undefined') {
-          pnr.system = 'sncf'
-        }
-        return pnr
-      })
-
-      // We try to find the list of the systems. there will be one
-      // bankoperation/proof/system
-      const systems = linkedPNR.reduce((sys, pnr) => {
-        if (sys.indexOf(pnr.system) === -1) {
-          return sys.concat(pnr.system)
-        }
-        return sys
-      }, [])
-
-      // Calculate the amount of each system because their is one operation per
-      // system.
-      for (const system of systems) {
-        const bill = {
-          pdfurl: proof.url,
-          type: 'train',
-          vendor: 'Trainline',
-          system,
-          date: moment(proof.created_at).hours(0)
-                                        .minutes(0)
-                                        .seconds(0)
-                                        .milliseconds(0)
-        }
-
-        // Get the list of refunds for the current bill
-        let refundID = []
-        refundID = linkedPNR.filter(pnr => pnr.system === system).reduce(
-          (list, pnr) => list.concat(pnr.after_sales_log_ids), []
-        )
-        let listRefund = []
-        listRefund = refundID.reduce((list, id) => list.concat(
-          data.after_sales_logs.find(asl => asl.id === id)), []
-        )
-
-        if (proof.type === 'purchase') {
-          // Compute the sum of refunds for the current bill
-          const reinboursedAmount = listRefund.reduce(
-            (sum, rb) => sum - rb.added_cents + rb.refunded_cents, 0
-          )
-          // We compute the amount of not reimbursed trips.
-          const paidAmount =
-            linkedPNR.filter(pnr => pnr.system === system).reduce(
-              (sum, p) => sum + p.cents, 0
-            )
-          // Get the the sum of penalties
-          const penaltiesAmount = listRefund.reduce(
-            (sum, rb) => sum + rb.penalty_cents, 0)
-          bill.amount = (paidAmount + reinboursedAmount + penaltiesAmount) / 100
-        } else {
-          // Find the unique Refund based on the emission date
-          const refund = listRefund.find(
-            refund => refund.date === proof.created_at
-          )
-          bill.amount = (refund.refunded_cents - refund.added_cents) / 100
-          bill.isRefund = true
-        }
-
-        bills.push(bill)
-      }
+      bills.push(bill)
     }
+  }
 
-    const filteredBills = []
-    // Recombine the bill list so that each entry.url is unique
-    for (const bill of bills) {
-      // Ensure the bill is not already in the list.
-      const sameUrlBills = filteredBills.filter(b =>
-          (b.pdfurl === bill.pdfurl && b.system === bill.system))
-      if (sameUrlBills.length === 0) {
-        const sameBill = bills.filter(b => (b.pdfurl === bill.pdfurl))
-                              .filter(b => (b.system === bill.system))
-        const newBill = {
-          amount: sameBill.reduce((amount, b) => (amount + b.amount), 0),
-          fileurl: bill.pdfurl,
-          date: bill.date.toDate(),
-          type: 'transport',
-          vendor: 'Trainline',
-          filename: getFileName(bill.date)
-        }
-        if (typeof bill.isRefund !== 'undefined') {
-          newBill.isRefund = bill.isRefund
-        }
-        filteredBills.push(newBill)
+  const filteredBills = []
+  // Recombine the bill list so that each entry.url is unique
+  for (const bill of bills) {
+    // Ensure the bill is not already in the list.
+    const sameUrlBills = filteredBills.filter(b =>
+        (b.pdfurl === bill.pdfurl && b.system === bill.system))
+    if (sameUrlBills.length === 0) {
+      const sameBill = bills.filter(b => (b.pdfurl === bill.pdfurl))
+                            .filter(b => (b.system === bill.system))
+      const newBill = {
+        amount: sameBill.reduce((amount, b) => (amount + b.amount), 0),
+        fileurl: bill.pdfurl,
+        date: bill.date.toDate(),
+        type: 'transport',
+        vendor: 'Trainline',
+        filename: getFileName(bill.date)
       }
+      if (typeof bill.isRefund !== 'undefined') {
+        newBill.isRefund = bill.isRefund
+      }
+      filteredBills.push(newBill)
     }
+  }
 
-    filteredBills.sort((a, b) => a.date < b.date ? 1 : -1)
+  filteredBills.sort((a, b) => a.date < b.date ? 1 : -1)
 
-    resolve(filteredBills)
-  })
+  return filteredBills
 }
 
 function getFileName (date) {
