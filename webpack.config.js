fa9cb703fcc36cab314d849fc5bdfe4f10933395
doubleface <christophe@cozycloud.cc>
2018-05-09 09:16:46 +0200

    upgrade packages + packaging like templates

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 var path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
+const entry = require('./package.json').main
 
 module.exports = {
-  entry: require('./package.json').main,
+  entry,
   target: 'node',
+  mode: 'none',
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'index.js'
@@ -12,8 +14,9 @@ module.exports = {
     new CopyPlugin([
       { from: 'manifest.konnector' },
       { from: 'package.json' },
-      { from: 'LICENSE' },
-      { from: 'assets' }
+      { from: 'README.md' },
+      { from: 'assets' },
+      { from: 'LICENSE' }
     ])
   ]
 }
